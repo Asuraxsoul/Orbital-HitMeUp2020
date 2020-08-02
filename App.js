744d95d60a2af78c16103dc43b3985ab8bcc54a4
Asuraxsoul <54731174+Asuraxsoul@users.noreply.github.com>
2020-08-02 12:10:20 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './src/Components/Route/Navigation';
+import HomeScreenDrawer from './src/Components/Home/HomeScreenDrawer';
+import Profile from './src/Components/ProfilePage/Profile';
+
+export default function App() {
+  return (
+    <Navigation />
+  )
+}
